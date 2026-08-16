9a2700611de2693bd4bf0f1d872d450f514e16a1
johndoe@example.com <johndoe@example.com>
2026-08-15 23:17:28 -0600

    Initial Commmit

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,11 @@
+return {
+  { import = "lazyvim.plugins.extras.dap.core" },
+
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.java" },
+}
