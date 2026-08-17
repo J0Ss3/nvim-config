1fa465e0b628161f58496d985d63177a3437d175
Jose F <jfvargascarrasco03@gmail.com>
2026-08-16 22:09:35 -0600

    Update spaces and indent to tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_check_order = false
+
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
