da882d506786ffb7272711261dbd6cb59e7987e4
Jose F <jfvargascarrasco03@gmail.com>
2026-08-24 23:14:13 -0600

    Refactor colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,13 @@
 return {
-  {
-    "tiagovla/tokyodark.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function(_, opts)
-      require("tokyodark").setup(opts)
-    end,
-  },
-  { "rebelot/kanagawa.nvim" },
-  { "ayu-theme/ayu-vim" },
-  { "nuvic/flexoki-nvim", name = "flexoki" },
-  { "wtfox/luna.nvim", lazy = true, priority = 1000, opts = {} },
+    -- Custom colorschemes
+    { "tiagovla/tokyodark.nvim", lazy = false, priority = 1000, opts = {}, config = function(_, opts) require("tokyodark").setup(opts) end, },
+    { "rebelot/kanagawa.nvim" },
+    { "ayu-theme/ayu-vim" },
+    { "nuvic/flexoki-nvim", name = "flexoki" },
+    { "wtfox/luna.nvim", lazy = true, priority = 1000, opts = {} },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+    { "yorik1984/newpaper.nvim", lazy = true, opts = { style = "dark" }, },
 
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
-  {
-    "yorik1984/newpaper.nvim",
-    lazy = true,
-    opts = { style = "dark" },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "luna" },
-  },
+    -- Set colorscheme
+    { "LazyVim/LazyVim", opts = { colorscheme = "luna" }, },
 }
